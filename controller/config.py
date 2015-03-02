5ee786eef4852135bbf6ea17270147ed58683c53
caeman <toombs.caeman@gmail.com>
2015-03-02 16:34:00 -0500

    updating config file

--- controller/config.py
+++ controller/config.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python2.7
 import os, sys
 
-# Project root is considered the directory this is run in. CSAssess/controller
-# This will allow all files below this to access anything within the whole tree
-# import using a relative path from the root, replacing / with .
-
-# In the file, include this header
-#	import sys
-#	from PROJECT_ROOT import PROJECT_ROOT
-#	sys.path.insert(0,PROJECT_ROOT)
-
-
 abspath = lambda *p: os.path.abspath(os.path.join(*p))
+
 PROJECT_ROOT = abspath(os.path.dirname(__file__))
 #print(PROJECT_ROOT)
 
@@ -22,7 +13,9 @@ for dirName, subdirList, fileList in os.walk(PROJECT_ROOT):
 	print(dirName)
 	with open(os.path.abspath(dirName) + "/PROJECT_ROOT.py", "w+") as f:
 		f.write("#!/usr/bin/python2.7\n")
-		f.write("PROJECT_ROOT='%s'" % (PROJECT_ROOT))
+		f.write("import sys\n")
+		f.write("PROJECT_ROOT='%s'\n" % (PROJECT_ROOT))
+		f.write("sys.path.insert(0,PROJECT_ROOT)")
 	with open(os.path.abspath(dirName) + "/__init__.py", "w+") as f:
 		f.write("")
 
