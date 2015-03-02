2bae3048f9e1ef2e7f1814a7d4f158b71d54bd1d
Ebube Chuba <chuba15e@ncssm.edu>
2015-03-02 14:52:03 -0500

    Testing creating answer JSON

--- controller/test.py
+++ controller/test.py
@@ -4,6 +4,9 @@ import constants
 import sys
 
 import viewAssessment
+import viewAnswer
+import viewJob
+import viewTestCase
 import viewQuestion
 import viewTopic
 import viewSection
@@ -16,6 +19,9 @@ import editQuestion
 import editTopic
 import editSection
 import editCourse
+import editComment
+import editUser
+
 
 from assessment import Assessment
 from question import Question
@@ -23,5 +29,5 @@ from topic import Topic
 from section import Section
 from course import Course
 from user import User
+from comment import Comment
 
-print(viewComment.byID(1))
