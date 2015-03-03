303a6793be34f2d4c28c762806fab90ef79d972a
zheng16m <zheng16m@ncssm.edu>
2015-03-03 16:24:41 -0500

    Admin page plus javascript
    Still missing addCourse

--- view/public_html/js/addUser.js
+++ view/public_html/js/addUser.js
@@ -2,10 +2,19 @@ function submitUser()
 {
 	var username = document.getElementById("usernameField").value;
 	var password = document.getElementById("passwordField").value;
+	var password2 = document.getElementById("passwordField2").value;
 	var first_name = "";
 	var last_name = "";
 	var name = document.getElementById("nameField").value;
-	var role = document.getElementById("roleField").value;
+	var roles = document.getElementsByName("roleField");
+	var role = "";
+	for (var i=0; i<roles.length; i++)
+	{
+		if(roles[i].checked)
+		{
+			role += "," + roles[i].value;
+		}
+	}
 	name = name.split(" ");
 	first_name = name[0];
 	last_name = name[1];
@@ -13,6 +22,12 @@ function submitUser()
 	{
 		alert("Please fill in all the fields!");
 	}
+	if (password!=password2)
+	{
+		alert("Your passwords do not match. Please retype your passwords.");
+		throw new Error("Passwords do not match");
+	}
+	role = role.substring(1);
 	var toSend = {"username": username, "password": password, "first_name": first_name, "last_name": last_name, "role": role};
 	//todo in later versions: add actual validation w/ tokens
 	toSend.token = "token-standin";
@@ -25,4 +40,9 @@ function submitUser()
         dataType: "json",
         success: console.log("Success!")
 	});
+}
+
+function showSelectedValues()
+{
+$("input[name=roleField]:checked").map(function(){return this.value;}).get().join(",");
 }
