757349b7af0489c1ab30c35e71e779dfe7d22242
speakerforthecats <decker16b@ncssm.edu>
2015-03-03 09:24:42 -0500

    Added a search functionality that runs (which will be removed)

--- view/public_html/js/database.js
+++ view/public_html/js/database.js
@@ -1,41 +1,61 @@
-function searchDatabase()
-{
-	var questions = parseJSON("/test/test.json");
-	var topic = document.getElementById("topic");
-	var difficulty = document.getElementById("difficulty");
-	var keyword = document.getElementById("practiceKeyword");
-	for (var i = 0; i < questions.length; i++)
-	{
-		var topic_id = questions[i].topic;
-		if (topic != topic_id)
-		{
-			questions.splice(i, 1);
+function getExternal(filepath, callback) { //ajax request function
+	var ajax = new XMLHttpRequest(); 
+	ajax.onreadystatechange = function () {
+		if (ajax.readyState === 4 && ajax.status === 200) {
+			callback(ajax.responseText);
 		}
-	}
+	};
+	ajax.open("GET", filepath, "true");
+	ajax.send();
+}
+function searchDatabase(text)
+{
+	//todo: find out how topic will be gotten and figure out how to parse it
+	var questions = JSON.parse(text);
+	//var topic = document.getElementById("topic").value;
+	var difficulty = document.getElementById("difficulty").value;
+	var keyword = document.getElementById("practiceKeyword").value;
+	var i = 0;
+	var j = 0;
+	//for (var i = 0; i < questions; i++)
+	//{
+	//	var topic_id = questions[i].topic;
+	//	if (topic != topic_id)
+	//	{
+	//		questions.splice(i, 1);
+	//	}
+	//}
+	var length = Object.keys(questions).length;
 	if (difficulty != "")
 	{
-		for (i = 0; i < questions.length; i++)
+		console.log("Difficulty check running!");
+		for (i = 0; i < length; i++)
 		{
 			var difficulty_id = questions[i].difficulty;
 			if (difficulty != difficulty_id)
 			{
-				questions.splice(i, 1);
+				delete questions[i];
 			}
 		}
 	}
 	if (keyword != "" && keyword != null)
 	{
 		keyword = keyword.split(" ");
-		if ((typeof keyword[1]) == "undefined")
+		if ((typeof keyword[1]) != "undefined")
 		{
-			document.getElementById("error").text = "Multiple keywords are not supported. Please enter 1 keyword";
+			document.getElementById("error").textContent = "Multiple keywords are not supported. Please enter 1 keyword";
 			return 0;
 		}
-		keyword[0] = keyword;
-		for (i = 0; i < questions.length; i++)
+		keyword = keyword[0];
+		//todo: implement partial string matching
+		for (i = 0; i < length; i++)
 		{
+			if ((typeof questions[i]) == 'undefined')
+			{
+				continue;
+			}
 			var question_content = questions[i].content;
-			question_content = question_content.split(" ");
+			question_content = question_content.split(" ");																			
 			var contains = 0;
 			for (j = 0; j < question_content.length; j++)
 			{
@@ -47,23 +67,55 @@ function searchDatabase()
 			}
 			if (contains != 1)
 			{
-				questions.splice(i, 1);
+				delete questions[i];
 			}
 		}
 	}
-	var data = ["language", "difficulty", "last_given", "content", "topic"]
+	document.getElementById("error").textContent = "";
+	var container = document.getElementById("database_container");
+	while (container.firstChild) 
+	{
+  		container.removeChild(container.firstChild);
+	}
+	var data = ["difficulty", "language", "last_given", "topic", "content"];
+	var data2 = ["Difficulty", "Language", "Last Given", "Topic", "Content"];
 	var database = document.createElement("TABLE");
 	database.setAttribute("id","database"); //creates table to keep the database in
-	for (i = 0; i < questions.length; i++) 
+	database.setAttribute("class","table table-bordered");
+	var thead = document.createElement("THEAD");
+	var tr = document.createElement("TR");
+	for (j = 0; j < 5; j++) 
 	{
-		var tr = document.createElement("TR");
-		for (var j = 0; j < 5; j++)
+    	var th = document.createElement("TH");
+    	th.textContent = data2[j];
+    	tr.appendChild(th);
+    }
+    thead.appendChild(tr);
+    database.appendChild(thead);
+    var tbody = document.createElement("TBODY");
+	for (i = 0; i < length; i++) 
+	{
+		if ((typeof questions[i]) == 'undefined')
 		{
-			var td = document.createElement("TD");
-			td.setAttribute("text", question[i].data[j])
-			tr.appendChild(td);
+			continue;
 		}
-		database.appendChild(tr);
+		var tr = document.createElement("TR");
+		for (j = 0; j < 5; j++) 
+		{
+    		var td = document.createElement("TD");
+    		td.textContent = questions[i][data[j]];
+    		tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    database.appendChild(tbody);
+    document.getElementById("database_container").appendChild(database);
+}
+function submitQuery(e)
+{
+	if (e.keycode == 13)
+	{
+		getExternal('/view/public_html/js/test/test.json', searchDatabase); 
+		return false;
 	}
-	document.getElementById("database_container").appendChild(database);
 }
