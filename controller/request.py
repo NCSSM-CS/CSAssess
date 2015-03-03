d4d76f2cbe2bdd73438d137927193513dc745e13
murray16s <murray16s@ncssm.edu>
2015-03-03 16:33:13 -0500

    request.py errorless. UNFINISHED

--- controller/request.py
+++ controller/request.py
@@ -1,13 +1,28 @@
-#!/usr/bin/env pyhton2.6
+#!/usr/bin/env python2.6
 
 """
 created_by: Ebube Chuba
 create_date: 3/3/2015
-last_modified_by: Ebube Chuba
+last_modified_by: Samuel Murray
 last_modified_date: 3/3/2015
 """
 
+"""
+TODO:
+    -Add all "requestType" use cases.
+"""
 #imports
-#import requests
-#r = requests.get("http://website.com")
-#r.json()
+import cgi
+import cgitb
+import json
+
+#cgitb.enable()
+
+#Next Line is a test case for toFile generation
+#form = {"requestType" : "addUser"}
+
+#form = cgi.FieldStorage()
+toFile = ""
+#toFile is a file that we will eventually pass the JSON into
+toFile = form["requestType"] + ".py"
+#print(toFile)
