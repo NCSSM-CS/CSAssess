f82addf5cd5230bd2bfb9ebc778ac42bb3144cdf
whitsett15c <whitsett15c@ncssm.edu>
2015-03-03 15:38:08 -0500

    Fixed language change bug

--- view/public_html/js/mode.js
+++ view/public_html/js/mode.js
@@ -1,42 +1,57 @@
 //CEW 
+var isCalled=false;
+var editor;
 function returnValues(val)
 {
   //val - value of dropdown menu containing languages
   //result - updates textarea to be language specific
-
-	var languageArgs;
+  var languageArgs;
 
   languageArgs = {
     lineNumbers: true,
     matchBrackets: true
   }
 
-  //Assigns arguments to the language object
-  if (val == "Python")
+  
+  //Assigns arguments to the language object.
+  if (!isCalled)
   {
-    languageArgs["name"] = "python";
-    languageArgs["version"] = 3;
-    languageArgs["indentUnit"] = 4;
-  }
-  else if (val == "Java")
-  {
-    languageArgs["mode"] = "text/x-java";
-  } 
-  else if (val == "C")
-  {
-    languageArgs["mode"] = "text/x-csrc";
-  }
-  else if (val == "C++")
-  {
-    languageArgs["mode"] = "text/x-c++src";
-  }  
+    if (val == "Python")
+    {
+      languageArgs["name"] = "python";
+      languageArgs["version"] = 3;
+      languageArgs["indentUnit"] = 4;
+    }
+    else if (val == "Java")
+    {
+      languageArgs["mode"] = "text/x-java";
+    } 
+    else if (val == "C")
+    {
+      languageArgs["mode"] = "text/x-csrc";
+    }
+    else if (val == "C++")
+    {
+      languageArgs["mode"] = "text/x-c++src";
+    } 
+     
+    this.editor = CodeMirror.fromTextArea(
+      document.getElementById('codingArea'),
+      languageArgs);
+    isCalled = true;
+    //If the option is changed, deletes codemirror and initializes a new one
+    //This took 1.5 hours to figure out. I nearly cried.
+  }   else {
 
-  var editor = CodeMirror(function(elt) {
-    document.getElementById('codingArea').parentNode.replaceChild(
-      elt, document.getElementById('codingArea'));
-  }, languageArgs);
+    this.editor.toTextArea();
 
-return;
+    isCalled=false;
+
+    returnValues(val); 
+
+  }
+  
+  return;
 }
 
 function submitSubmission(){
