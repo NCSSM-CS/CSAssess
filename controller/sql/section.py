412f770fd2bb3576e7646a74059a3a11d7b67c0c
Luke Zeller <zeller16r@ncssm.edu>
2015-03-03 22:02:23 +0000

    functions added to section.py

--- controller/sql/section.py
+++ controller/sql/section.py
@@ -15,7 +15,7 @@ import json
 class Section:
     'Section object to hold attributes and functions for a section'
 
-    def __init__(self, id, created, created_by, course, year, term, period):
+    def __init__(self, id, created, created_by, course, year, term, period, active):
         """
         self       - the section in question
         id         - the id number of the section 'self' in the database
@@ -25,7 +25,8 @@ class Section:
         year       - the year that the section 'self' took place in
         term       - the term that the section 'self' took place in
         period     - the period that the section 'self' took place in
-
+	active     - bit specifying whether job is active or inactive
+	
         this function acts as the constructor to define a new section object
         """
         self.id         = id
@@ -35,9 +36,10 @@ class Section:
         self.year       = year
         self.term       = term
         self.period     = period
+	self.active     = active
 
     @classmethod
-    def noID(cls, created, created_by, course, year, term, period):
+    def noID(cls, created, created_by, course, year, term, period, active):
         """
         the parameters correspond with the parameters in the constructor above
 
@@ -47,7 +49,7 @@ class Section:
         this function acts as a second constructor where you have created an
         assessment that has not yet been assigned an id from the database
         """
-        return cls(None, created, created_by, course, year, term, period)
+        return cls(None, created, created_by, course, year, term, period, active)
 
     def __eq__(self, other):
         """
@@ -68,7 +70,8 @@ class Section:
         self.course     == other.course     and
         self.year       == other.year       and
         self.term       == other.term       and
-        self.period     == other.period)
+        self.period     == other.period     and
+	self.active     == other.active)
 
     def __str__(self):
         """
@@ -83,22 +86,60 @@ class Section:
         string += "id: "         + str(self.id)         + "\n"
         string += "created: "    + str(self.created)    + "\n"
         string += "created by: " + str(self.created_by) + "\n"
-        string += "\ncourse: "   + str(self.course)     + "\n"
+        string += "course: "   + str(self.course)     + "\n"
         string += "year: "       + str(self.year)       + "\n"
         string += "term: "       +     self.term        + "\n"
         string += "period: "     +     self.period      + "\n"
+	string += "active: "     + str(self.active)     + "\n"
 
         return string
 
     def add(self):
-        cnx = mysql.connector.connect(**getConfig())
+        
+	if self.id is not None:
+	    return
+	
+	cnx = mysql.connector.connect(**getConfig())
         cursor = cnx.cursor)
 
-        if 
+	insert = ("INSERT INTO section (id, created, created_by, course, year, term, period, active) VALUES (%s, '%s', %s, %s, %s, '%s', '%s', %s); SELECT LAST_INSERT_ID();" % (self.id, self.created, self.created_by.id, self.course.id, self.year, self.term, self.period, self.active))
+
+	cursor.execute(insert)
+
+	for(id) in cursor:
+	    self.id=id
 
         cnx.commit()
         cursor.close()
         cnx.close()
+
+    def update(self):
+        cnx = mysql.connector.connect(**getConfig())
+	cursor = cnx.cursor()
+
+	if self.id is not None:
+	    update = ("UPDATE job SET created = '%s', created_by = %s, course = %s, year = %s, term = '%s', period = '%s', active = %s;" % (self.created, self.created_by.id, self.course.id, self.year, self.term, self.period, self.active))
+
+	    cursor.execute(update)
+
+	cnx.commit()
+	cursor.close()
+	cnx.close()
+
+    def activate(self, bool):
+        cnx = mysql.connector.connect(**getConfig())
+	cursor = cnx.cursor()
+
+	if self.active is not None:
+
+	    self.active = int(bool)
+	    active = ("UPDATE section SET active=%s WHERE id=%s;" % (int(bool), self.id))
+
+	    cursor.execute()
+	
+	cnx.commit()
+	cursor.close()
+	cnx.close()
 
     @classmethod
     def get(self, search, active):
