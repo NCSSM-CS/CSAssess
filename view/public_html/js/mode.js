133e9733b8ff0a5af7b0c7aa853ef5b0ca38396d
whitsett15c <whitsett15c@ncssm.edu>
2015-03-03 10:38:41 -0500

    Added codemirror to problem page

--- view/public_html/js/mode.js
+++ view/public_html/js/mode.js
@@ -1,34 +1,53 @@
-function returnValues(val){
-	if (val == 0){
-		return {
-            lineNumbers: true,
-            name: "python",
-            version: 3,
-            indentUnit: 4,
-            matchBrackets: true
-          });
-	} else if (val == 1){
-		return {
-        lineNumbers: true,
-        matchBrackets: true,
-        mode: "text/x-java"
-      });
-	} else if (val == 2){
-		return {
-        lineNumbers: true,
-        matchBrackets: true,
-        mode: "text/x-csrc"
-      });
-	} else if (val == 3){
-		return {
-        lineNumbers: true,
-        matchBrackets: true,
-        mode: "text/x-c++src"
-      });
-	} else {
-		return {
-			lineNumbers: true,
-			matchBrackets: true;
-		}
-	}
+//CEW 
+function returnValues(val)
+{
+  //val - value of dropdown menu containing languages
+  //result - updates textarea to be language specific
+
+	var languageArgs;
+
+  languageArgs = {
+    lineNumbers: true,
+    matchBrackets: true
+  }
+
+  //Assigns arguments to the language object
+  if (val == "Python")
+  {
+    languageArgs["name"] = "python";
+    languageArgs["version"] = 3;
+    languageArgs["indentUnit"] = 4;
+  }
+  else if (val == "Java")
+  {
+    languageArgs["mode"] = "text/x-java";
+  } 
+  else if (val == "C")
+  {
+    languageArgs["mode"] = "text/x-csrc";
+  }
+  else if (val == "C++")
+  {
+    languageArgs["mode"] = "text/x-c++src";
+  }  
+
+  var editor = CodeMirror(function(elt) {
+    document.getElementById('codingArea').parentNode.replaceChild(
+      elt, document.getElementById('codingArea'));
+  }, languageArgs);
+
+return;
+}
+
+function submitSubmission(){
+  //result - Gets metadata and puts it into a JSON or list. Not sure yet
+  var submitData;
+  submitData = {
+    language: document.getElementById('languageSelect').value,
+    time: Date(),
+    problem: "1.1",
+    code: document.getElementById('codingArea').value
+  };
+
+  console.log(submitData);
 }
