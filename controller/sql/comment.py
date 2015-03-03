f56a1dfaa977e303332dce5521e8ee3f8d5b6f28
bent15m <bent15m@ncssm.edu>
2015-03-03 15:32:26 -0500

    comment.py updated

--- controller/sql/comment.py
+++ controller/sql/comment.py
@@ -3,16 +3,18 @@
 """
 created_by:         John Fang
 created_date:       3/2/2015
-last_modified_by:   John Fang
-last_modified date: 3/2/2015
+last_modified_by:   Matthew Bent (Bear)
+last_modified date: 3/3/2015
 """
 
 # imports
 import constants
 import json
-
+import mysql.connector
+from user import User
+from mysql_connecto_config import getConfig
 # classes
-class Comment:
+class Comment(object):
     'Comment object to hold attributes and functions for an assessment'
 
     def __init__(self, id, created, created_by, answer, content):
@@ -31,8 +33,80 @@ class Comment:
         self.created_by = created_by
         self.answer     = answer
         self.content    = content
+    def add(self):
+        
+        if self.id is not None:
+            return
+        
+        cnx = mysql.connector.connect(**getConfig())
+        cursor = cnx.cursor()
 
+        insert = ("INSERT INTO comment (id, created, created_by, answer_id, content, active) VALUES (%s, '%s', %s, '%s', '%s', %s); SELECT LAST_LAST_INSERT_ID();" %(self.id, self.created, self.created_by.id, self.answer_id, self.content, self.active))
+        cursor.execute(insert)
+
+        for (id) in cursor:
+            self.id=id
+
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+    
+    def edit(self):
+        
+        cnx = mysql.connector.connect(**getConfig())
+        cursor = cnx.cursor()
+
+        if self.id is not None:
+            update = ("UPDATE comment SET created = '%s', created_by = %s, answer_id = %s,  content = '%s' WHERE id = %s;" % (self.created, self.created_by.id, self.asnwer_id, self.content, self.id))
+            cursor.execute(update)
+
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+        
+    def activate(self, bool):
+        cnx = mysql.connector.connect(**getConfig())
+        cursor = cnx.cursor()
+
+	self.active = int(bool)
+        active = ("UPDATE comment SET active=%s WHERE id=%S;" % (int(bool), self.id))
+
+        cursor.execute(active)
+
+        cnx.commit()
+        cursor.close()
+        cnx.close()
+        
     @classmethod
+    def get(self, search="all", testActive=1):
+        cnx = mysql.connector.connect(**getcConfig())
+        cursor = cnx.cursor()
+
+        returnList = []
+        query = ""
+        if search == "all":
+            query = "SELECT * FROM comment"
+        elif type(search) is int:
+            query = ("SELECT * FROM comment WHERE id=%s" % (search))
+        elif type(search) is User:
+            query = ("SELECT * FROM comment WHERE created_by=%s" % (search))
+        elif type(search) is Answer: 
+            query = ("SELECT * FROM comment WHERE answer_id=%s" % (search))
+
+        query += " AND active =%s;" % (testActive)
+        cursor.execute(query)
+        for (id, created, created_by, answer_id, content, active) in cursor:
+            user = User.get(created_by)[0]
+            newComment = Comment(id, created, user, answer_id, content, active)
+            if newComment not in returnList:
+                returnList.append(newComment)
+        
+        cnx.commit()
+        cursor.close()
+        cnx.close
+
+        return returnList
+
     def noID(self, created, created_by, answer, content):
         """
         the parameters correspond with the parameters in the constructor above
