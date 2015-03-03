b8d06e6a69caf6912a1fc60ede0dadc17b18ddfa
speakerforthecats <decker16b@ncssm.edu>
2015-03-03 15:46:08 -0500

    Modified functionality of database.js to comply and added add user functionality

---
+++ view/public_html/js/addUser.js
@@ -0,0 +1,27 @@
+function submitUser()
+{
+	var username = document.getElementById("usernameField").value;
+	var password = document.getElementById("passwordField").value;
+	var first_name = "";
+	var last_name = "";
+	var name = document.getElementById("nameField").value;
+	var role = document.getElementById("roleField").value;
+	name = name.split(" ");
+	first_name = name[0];
+	last_name = name[1];
+	if (username == "" || username == null || password == "" || password == null || name == "" || name == null || role == "")
+	{
+		alert("Please fill in all the fields!");
+	}
+	var toSend = {"username": username, "password": password, "first_name": first_name, "last_name": last_name, "role": role};
+	//todo in later versions: add actual validation w/ tokens
+	toSend.token = "token-standin";
+	toSend.requestType = "addUser";
+	$.post(
+	{                                                 
+        url:"/cgi-bin/request.py",                                                 
+        data: toSend,
+        dataType: "json",
+        success: console.log("Success!")
+	});
+}
