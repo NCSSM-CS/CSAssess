ff5c6b06244276cf760e3e34176cdce2f5c2518d
aravindez <ezhilarasan15a@ncssm.edu>
2015-03-03 16:25:05 -0500

    nm

--- controller/sql/assessment.py
+++ controller/sql/assessment.py
@@ -3,8 +3,8 @@
 """
 created_by:         Micah Halter
 created_date:       2/28/2015
-last_modified_by:   John Fang
-last_modified date: 3/2/2015
+last_modified_by:   EZ
+last_modified date: 3/3/2015
 """
 
 # imports
