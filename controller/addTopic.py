6a48191dea51c8f486edf4e26a29096094cc6e6b
anindamanocha <manocha15a@ncssm.edu>
2015-03-05 14:53:15 -0500

    fixed addTopic

--- controller/addTopic.py
+++ controller/addTopic.py
@@ -3,25 +3,24 @@
 """
 created_by:         John Fang
 created_date:       3/4/2015
-last_modified_by:   John Fang
-last_modified_date: 3/4/2015
+last_modified_by:   Aninda Manocha
+last_modified_date: 3/5/2015
 """
 
 # imports
 import constants
+import utils
 import json
-from sql.user import User
 from sql.topic import Topic
 from sql.session import Session
 
-#Format of answer
-#name: "string"
-#active: boolean 
+#Format of topic
+#name: "string" 
 
 def iChooseU(json):
     thisUser = findUser()
 
-    name = json("name")
+    name = json["name"]
 
     newTopic = Topic.noID(None, thisUser, name, ACTIVE)
     newTopic.add()
