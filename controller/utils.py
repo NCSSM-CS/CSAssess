5b41b9ef83ac1823d8a2b062a4547eb7bedf05c3
Ebube Chuba <chuba15e@ncssm.edu>
2015-03-05 15:37:05 -0500

    IP address work nowgit add utils.py git add utils.py git add utils.py

--- controller/utils.py
+++ controller/utils.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
+import os
 from sql.user import User
 from sql.session import Session
 
 def findUser(json):
-    ipAddress = self.client_address[0]
+    ipAddress = os.environ["REMOTE_ADDR"]
     session = Session.get(json["session"], ipAddress)[0]
     thisUser = User.get(session[0])[0]
     if DEBUG > 1:
