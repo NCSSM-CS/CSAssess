cfe88bb27589be77b05928f3e351c83abcbf3cf6
caeman <toombs.caeman@gmail.com>
2015-03-05 11:21:09 -0500

    refactoring request.py

--- controller/request.py
+++ controller/request.py
@@ -1,118 +1,50 @@
 #!/usr/bin/env python3
 
-"""
-created_by: Ebube Chuba
-create_date: 3/3/2015
-last_modified_by: Ebube Chuba
-last_modified_date: 3/5/2015
-"""
-
-#imports
 import cgi
 import cgitb
 import json
 import sys
 import constants
-#import addTopic
-#import getTopic
-#import updateTopic
-#import activateTopic
-import getUser
-import addUser
-#import updateUser
-#import activateUser
-#import getAssessment
-#import addAssessment
-#import updateAssessment
-#import activateAssessment
-#import getQuestion
-#import addQuestion
-#import updateQuestion
-#import activateQuestion
-#import addAnswer
-#import getAnswer
-#import updateAnswer
-#import activateAnswer
-#import addCourse
-#import getCourse
-#import uppdateCourse
-#import activateCourse
-#import addSection
-#import getSection
-#import updateSection
-#import activateSection
+
+# if you find a better way to do this plz replace
 
 cgitb.enable()
 
 unprocessedForm = cgi.FieldStorage()
+
 toFile = ""
 #toFile is a file that we will eventually pass the JSON into
 toFile = unprocessedForm["requestType"]
 if constants.DEBUG > 0:
     print(toFile)
-# Add Topic requestTypes
-#if toFile == "getTopic":
-#    processedForm = getTopic.iChooseU(unprocessedForm)
-#elif toFile == "addTopic":
-#    processedForm = addTopic.iChooseU(unprocessedForm)
-#elif toFile == "updateTopic":
-#    processedForm = updateTopic.iChooseU(unprocessedForm)
-#elif toFile == "activateTopic":
-#    processedForm = activateTopic.iChooseU(unprocessedForm)
-# Add User requestTypes
-elif toFile == "getUser":
-    processedForm = getUser.iChooseU(unprocessedForm)
-elif toFile == "addUser":
-    processedForm = addUser.iChooseU(unprocessedForm)
-#elif toFile == "updateUser":
-#    processedForm = updateUser.iChooseU(unprocessedForm)
-#elif toFile == "activateUser":
-#    processedForm = activateUser.iChooseU(unprocessedForm)
-# Add Assessment requestTypes
-#elif toFile == "getAssessment":
-#    processedForm = getAssessment.iChooseU(unprocessedForm)
-#elif toFile == "addAssessment":
-#    processedForm = addAssessment.iCChooseU(unprocessedForm)
-#elif toFile == "updateAssessment":
-#    processedForm = updateAssessment.iChooseU(unprocessedForm)
-#elif toFile == "activateAssessment":
-#    processedForm = activateAssessment.iChooseU(unprocessedForm)
-# Add Question requestTypes
-#elif toFile == "getQuestion":
-#    processedForm = getQuestion.iChooseU(unprocessedForm)
-#elif toFile == "addQuestion":
-#    processedForm = addQuestion.iChooseU(unprocessedForm)
-#elif toFile == "updateQuestion":
-#    processedForm = updateQuestion.iChooseU(unprocessedForm)
-#elif toFile == "activateQuestion":
-#    processedForm = activateQuestion.iChooseU(unprocessedForm)
-# Add Answer requestTypes
-#elif toFile == "getAnswer":
-#    processedForm = getAnswer.iChooseU(unprocessedForm)
-#elif toFile == "addAnswer":
-#    processedForm = addAnswer.iChooseU(unprocessedForm)
-#elif toFile == "updateFile":
-#    processedForm = updateAnswer.iChooseU(unprocessedForm)
-#elif toFile == "activateAnswer"
-#    processedForm = activateAnswer.iChooseU(unprocessedForm)
-# Add Course requestTypes
-#elif toFile == "getCourse":
-#    processedForm = getCourse.iChooseU(unprocessedForm)
-#elif toFile == "addCourse":
-#    processedForm = addCourse.iChooseU(unprocessedForm)
-#elif toFile == "updateCourse":
-#    processedForm = updateCourse.iChooseU(unprocessedForm)
-#elif toFile == "activateCourse":
-#    processedForm = activateCourse.iChooseU(unprocessedForm)
-# Add Section requestTypes
-#elif toFile == "getSection":
-#    processedForm = getSection.iChooseU(unprocessedForm)
-#elif toFile == "addSection":
-#    processedForm = addSection.iChooseU(unprocessedForm)
-#elif toFile == "updateSection":
-#    processedForm = updateSection.iChooseU(unprocessedForm)
-#elif toFile == "activateSection":
-#    processedForm = activateSection.iChooseU(unprocessedForm)
+
+objectList = [ "User", "Assignment", "Section", "Course", "Topic", "Question"] #TODO add in all objects
+verbList   = [ "add", "get", "update", "activate", "login" ]
+
+# currVerb/Object can only come from the provided lists
+# protects against injections
+currVerb = ""
+currObject = ""
+for i in verbList:
+    if i in toFile:
+        currVerb = i
+
+for i in objectList:
+    if i in toFile:
+        currObject = i
+
+if currVerb.equals("login"):
+    # is login handled here?
+    processedForm = "{success:failure}"
+else
+    if currVerb != "" and currObject != "":
+        verbObject = currVerb + currObject
+        eval("import " + verbObject)
+        toEval = verbObject + ".iChooseU(unprocessedForm)"
+        processedForm = eval(toEval)
+    else
+        # malformed tags go here?
+        processedForm = "{success:failure}"
 
 print("Content-Type: application/json; charset=utf-8")
 print()
