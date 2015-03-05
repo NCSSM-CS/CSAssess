fb151d6a14de0ab877c89b3a233d0d847f83e2cd
murray16s <murray16s@ncssm.edu>
2015-03-05 10:01:44 -0500

    Updating request.py

--- controller/request.py
+++ controller/request.py
@@ -13,22 +13,34 @@ import cgitb
 import json
 import sys
 import constants
-#import addTopic
-#import getTopic
+import addTopic
+import getTopic
 #import updateTopic
 #import activateTopic
-#import getUser
-#import addUser
+import getUser
+import addUser
 #import updateUser
 #import activateUser
-#import getAssessment
-#import addAssessment
+import getAssessment
+import addAssessment
 #import updateAssessment
 #import activateAssessment
 import getQuestion
 import addQuestion
 #import updateQuestion
 #import activateQuestion
+import addAnswer
+import getAnswer
+#import updateAnswer
+#import activateAnswer
+import addCourse
+import getCourse
+#import uppdateCourse
+#import activateCourse
+import addSection
+import getSection
+#import updateSection
+#import activateSection
 
 cgitb.enable()
 
@@ -39,28 +51,28 @@ toFile = unprocessedForm["requestType"]
 if constants.DEBUG > 0:
     print(toFile)
 # Add Topic requestTypes
-#if toFile == "getTopic":
-#    processedForm = getTopic.iChooseU(unprocessedForm)
-#elif toFile == "addTopic":
-#    processedForm = addTopic.iChooseU(unprocessedForm)
+if toFile == "getTopic":
+    processedForm = getTopic.iChooseU(unprocessedForm)
+elif toFile == "addTopic":
+    processedForm = addTopic.iChooseU(unprocessedForm)
 #elif toFile == "updateTopic":
 #    processedForm = updateTopic.iChooseU(unprocessedForm)
 #elif toFile == "activateTopic":
 #    processedForm = activateTopic.iChooseU(unprocessedForm)
 # Add User requestTypes
-#elif toFile == "getUser":
-#    processedForm = getUser.iChooseUunprocessedForm)
-#elif toFile == "addUser":
-#    processedForm = addUser.iChooseU(unprocessedForm)
+elif toFile == "getUser":
+    processedForm = getUser.iChooseU(unprocessedForm)
+elif toFile == "addUser":
+    processedForm = addUser.iChooseU(unprocessedForm)
 #elif toFile == "updateUser":
 #    processedForm = updateUser.iChooseU(unprocessedForm)
 #elif toFile == "activateUser":
 #    processedForm = activateUser.iChooseU(unprocessedForm)
 # Add Assessment requestTypes
-#elif toFile == "getAssessment":
-#    processedForm = getAssessment.iChooseU(unprocessedForm)
-#elif toFile == "addAssessment":
-#    processedForm = addAssessment.iCChooseU(unprocessedForm)
+elif toFile == "getAssessment":
+    processedForm = getAssessment.iChooseU(unprocessedForm)
+elif toFile == "addAssessment":
+    processedForm = addAssessment.iCChooseU(unprocessedForm)
 #elif toFile == "updateAssessment":
 #    processedForm = updateAssessment.iChooseU(unprocessedForm)
 #elif toFile == "activateAssessment":
@@ -74,6 +86,33 @@ elif toFile == "addQuestion":
 #    processedForm = updateQuestion.iChooseU(unprocessedForm)
 #elif toFile == "activateQuestion":
 #    processedForm = activateQuestion.iChooseU(unprocessedForm)
+# Add Answer requestTypes
+elif toFile == "getAnswer":
+    processedForm = getAnswer.iChooseU(unprocessedForm)
+elif toFile == "addAnswer":
+    processedForm = addAnswer.iChooseU(unprocessedForm)
+#elif toFile == "updateFile":
+#    processedForm = updateAnswer.iChooseU(unprocessedForm)
+#elif toFile == "activateAnswer"
+#    processedForm = activateAnswer.iChooseU(unprocessedForm)
+# Add Course requestTypes
+elif toFile == "getCourse":
+    processedForm = getCourse.iChooseU(unprocessedForm)
+elif toFile == "addCourse":
+    processedForm = addCourse.iChooseU(unprocessedForm)
+#elif toFile == "updateCourse":
+#    processedForm = updateCourse.iChooseU(unprocessedForm)
+#elif toFile == "activateCourse":
+#    processedForm = activateCourse.iChooseU(unprocessedForm)
+# Add Section requestTypes
+elif toFile == "getSection":
+    processedForm = getSection.iChooseU(unprocessedForm)
+elif toFile == "addSection":
+    processedForm = addSection.iChooseU(unprocessedForm)
+#elif toFile == "updateSection":
+#    processedForm = updateSection.iChooseU(unprocessedForm)
+#elif toFile == "activateSection":
+#    processedForm = activateSection.iChooseU(unprocessedForm)
 
 print("Content-Type: application/json; charset=utf-8")
 print()
