4add27ce03b983d180366f5ae32ea5e11c251b64
zheng16m <zheng16m@users.noreply.github.com>
2015-03-05 10:55:21 -0500

    Update and rename view/public_html/js to view/public_html/js/addCourse.js

---
+++ view/public_html/js/addCourse.js
@@ -0,0 +1,25 @@
+function submitCourse()
+{
+	var name = document.getElementById("courseNameArea").value;
+	var id = document.getElementById("courseIDArea").value;
+	if (name == "" || name == null || id == "" || id == null)
+	{
+		alert("Please fill in all the fields!");
+	}
+        var token = getCookie("token");
+<<<<<<< HEAD
+	var toSend = {"name": name, "courseCode": id, "session": token};
+	toSend.requestType = "addUser";
+=======
+	var toSend = {"name": name, "ID": id, "topics": topics, "session": token};
+	toSend.requestType = "addCourse";
+>>>>>>> origin/master
+	console.log(toSend);
+        //$.post(urlToSubmitTo, dataToSubmit, successFunctionToRunOnReturn, expectedReturnType)
+        $.post("/cgi-bin/request.py", toSend, console.log("done"), "json");
+}
+
+function showSelectedValues()
+{
+$("input[name=roleField]:checked").map(function(){return this.value;}).get().join(",");
+}
