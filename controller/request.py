2116a93faf45bd9683b3a3d260f1e0c990b5aa2a
caeman <toombs.caeman@gmail.com>
2015-03-05 13:59:14 -0500

    fixing request.py imports

--- controller/request.py
+++ controller/request.py
@@ -6,45 +6,54 @@ import json
 import sys
 import constants
 
-# if you find a better way to do this plz replace
-
+#=== Init ===#
 cgitb.enable()
 
 unprocessedForm = cgi.FieldStorage()
 
-toFile = ""
-#toFile is a file that we will eventually pass the JSON into
-toFile = unprocessedForm["requestType"]
-#if constants.DEBUG > 0:
-#    print(toFile)
+requestType = ""
+requestType = unprocessedForm["requestType"]
 
 objectList = [ "User", "Assignment", "Section", "Course", "Topic", "Question"] #TODO add in all objects
 verbList   = [ "add", "get", "update", "activate", "login" ]
-
-# currVerb/Object can only come from the provided lists
-# protects against injections
 currVerb = ""
 currObject = ""
+
+#if constants.DEBUG > 0:
+#    print(requestType)
+
+
+#=== Body ===#
+# currVerb/Object can only come from the provided lists
+# protects against injections
+# safe-ish
 for i in verbList:
-    if i in toFile.value:
+    if i in requestType.value:
         currVerb = i
 
 for i in objectList:
-    if i in toFile.value:
+    if i in requestType.value:
         currObject = i
 
 if currVerb == "login":
-    # is login handled here?
+    #TODO
+    # is login handled in this file?
     processedForm = '{"success":"failure"}'
 else:
     if currVerb != "" and currObject != "":
         verbObject = currVerb + currObject
-        module = __import__(verbObject, globals(), locals(), [], -1)
+        # this is supposed to be emulating "eval('import ' + verbObject)"
+        #                   name      |  add to | add to | import [] from | level?
+        module = __import__(verbObject, globals(), locals(), [], 0)
         processedForm = exec("module." + verbObject + ".iChooseU(unprocessedForm)")
     else:
         # malformed tags go here?
+        #TODO
         processedForm = '{"success":"failure"}'
 
+#=== Return JSON ===#
+# format
 print("Content-Type: application/json; charset=utf-8")
 print()
+# content
 print(processedForm)
