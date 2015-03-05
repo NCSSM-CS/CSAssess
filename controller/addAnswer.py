6289707d898444c0ee116711e291b9f00aab1748
anindamanocha <manocha15a@ncssm.edu>
2015-03-05 17:01:08 -0500

    fixed addAnswer

--- controller/addAnswer.py
+++ controller/addAnswer.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 """
 created_by:         Aninda Manocha
 created_date:       3/4/2015
 last_modified_by:   Aninda Manocha
-last_modified date: 3/4/2015
+last_modified date: 3/5/2015
 """
 
 # imports
@@ -18,18 +18,19 @@ from sql.session import Session
 
 #Format of answer -AM
 #requestType: answer
-#question: question.id (Integer)
+#question: Question
 #content: "string"
 #isSolution: boolean
 
 def iChooseU(json):
-    thisUser = findUser()
+    thisUser = utils.findUser(json)
     
     question = json["question"]
+    theQuestion = Question.get(question["id"])[0]
     content = json["content"]
     isSolution = True 
 
-    newAnswer = Answer.noID(None, thisUser, question, None, content, isSolution, ACTIVE)
+    newAnswer = Answer.noID(None, thisUser, theQuestion, None, content, isSolution, ACTIVE)
     newAnswer.add()
 
-    return successJson()
+    return utils.successJson(json)
