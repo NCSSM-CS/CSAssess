94849b97e41ade9447a9bc0a0a557ee5296d56b9
caeman <toombs.caeman@gmail.com>
2015-03-05 14:32:28 -0500

    refactoring request

--- controller/request.py
+++ controller/request.py
@@ -6,54 +6,52 @@ import json
 import sys
 import constants
 
-#=== Init ===#
-cgitb.enable()
-
-unprocessedForm = cgi.FieldStorage()
-
-requestType = ""
-requestType = unprocessedForm["requestType"]
-
-objectList = [ "User", "Assignment", "Section", "Course", "Topic", "Question"] #TODO add in all objects
-verbList   = [ "add", "get", "update", "activate", "login" ]
-currVerb = ""
-currObject = ""
-
-#if constants.DEBUG > 0:
-#    print(requestType)
-
-
-#=== Body ===#
-# currVerb/Object can only come from the provided lists
-# protects against injections
-# safe-ish
-for i in verbList:
-    if i in requestType.value:
-        currVerb = i
-
-for i in objectList:
-    if i in requestType.value:
-        currObject = i
-
-if currVerb == "login":
-    #TODO
-    # is login handled in this file?
-    processedForm = '{"success":"failure"}'
-else:
-    if currVerb != "" and currObject != "":
-        verbObject = currVerb + currObject
-        # this is supposed to be emulating "eval('import ' + verbObject)"
-        #                   name      |  add to | add to | import [] from | level?
-        module = __import__(verbObject, globals(), locals(), [], 0)
-        processedForm = exec("module." + verbObject + ".iChooseU(unprocessedForm)")
+def processRequest(unprocessedForm):
+    """ Receives cgi.FieldStorage() and returns JSON to be printed"""
+    
+    if unprocessedForm.has_key("requestType"):
+        requestType = unprocessedForm.getvalue("requestType")
+        return '{"success":"success?"}' # testing
+        
     else:
-        # malformed tags go here?
-        #TODO
+        # received form contains no requestType
+        return '{"success":"failure"}'
+
+
+    objectList = [ "User", "Assignment", "Section", "Course", "Topic", "Question"] #TODO add in all objects
+    verbList   = [ "add", "get", "update", "activate", "login" ]
+    currVerb = ""
+    currObject = ""
+    
+    # currVerb/Object can only come from the provided lists
+    # protects against injections
+    # safe-ish
+    for i in verbList:
+        if i in requestType:
+            currVerb = i
+    for i in objectList:
+        if i in requestType:
+            currObject = i
+    
+    if currVerb == "login":
+        #TODO is login handled in this file?
         processedForm = '{"success":"failure"}'
+    else:
+        if currVerb != "" and currObject != "":
+            verbObject = currVerb + currObject
+            # this is supposed to be emulating "eval('import ' + verbObject)"
+            #                   name      |  add to | add to | import [] from | level?
+            module = __import__(verbObject, globals(), locals(), [], 0)
+            processedForm = exec("module." + verbObject + ".iChooseU(unprocessedForm)")
+        else:
+            #TODO malformed tags go here?
+            processedForm = '{"success":"failure"}'
 
 #=== Return JSON ===#
-# format
+cgitb.enable()
+unprocessedForm = cgi.FieldStorage()
+
+
 print("Content-Type: application/json; charset=utf-8")
 print()
-# content
-print(processedForm)
+print(processRequest(unprocessedForm))
