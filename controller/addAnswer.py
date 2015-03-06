0c15e6975f151380adb2b0a3ba147583837d0e2c
Ebube Chuba <chuba15e@ncssm.edu>
2015-03-06 11:34:27 -0500

    Fixed constant variable call

--- controller/addAnswer.py
+++ controller/addAnswer.py
@@ -3,7 +3,7 @@
 """
 created_by:         Aninda Manocha
 created_date:       3/4/2015
-last_modified_by:   Aninda Manocha
+last_modified_by:   Ebube Chuba
 last_modified date: 3/6/2015
 """
 
@@ -29,7 +29,7 @@ def iChooseU(form):
     content = form["content"]
     isSolution = True 
 
-    newAnswer = Answer.noID(None, thisUser, thisQuestion, None, content, isSolution, ACTIVE)
+    newAnswer = Answer.noID(None, thisUser, thisQuestion, None, content, isSolution, constants.ACTIVE)
     newAnswer.add()
 
     return utils.successJson(form)
