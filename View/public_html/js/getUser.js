c04a34e654ab2e55c532de14a508074ee3812450
Andrew Spencer <spencer16a@ncssm.edu>
2015-03-06 09:30:02 -0500

    getUser

--- View/public_html/js/getUser.js
+++ View/public_html/js/getUser.js
@@ -2,7 +2,7 @@
  * This is a file to test the functionality of getting a user. 
  */
 
-function doOnLoad() {
+function generateUserInfo() {
     var token = getCookie("token");
     var dataDef = {"requestType":"getUser", "session": token, "firstName": "Morgan", "lastName": "Freeman"};
     var urlDef = "/cgi-bin/CSAssess/request.py";
@@ -10,6 +10,21 @@ function doOnLoad() {
     $.post(urlDef, dataDef, showUser, dataTypeDef);
 }
 
-function showUser(user) {
-    alert(user.section);
+function showUser(users) {
+    var length = users.length;
+     var tbody = document.createElement("TBODY"); //gets all questions and puts them in table 
+	for (var i = 0; i < length; i++) 
+	{	
+		var tr = document.createElement("TR");
+                data = Object.getKeys(users[i]);
+		for (var j = 0; j < data.length; j++) 
+		{
+    		var td = document.createElement("TD");
+    		td.textContent = users[i][j];
+    		tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    database.appendChild(tbody);
+    document.getElementById("database_container").appendChild(database);
 }
