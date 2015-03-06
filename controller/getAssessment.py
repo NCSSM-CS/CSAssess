e3d7811ffd4946b2e286a49cff05593015b7b5ab
anindamanocha <manocha15a@ncssm.edu>
2015-03-06 15:25:56 -0500

    fixed formatting and imported utils

--- controller/getAssessment.py
+++ controller/getAssessment.py
@@ -3,50 +3,56 @@
 """
 created_by:         John Fang
 created_date:       3/4/2015
-last_modified_by:   Ebube Chuba
+last_modified_by:   Aninda Manocha
 last_modified_date: 3/6/2014
 """
 
 # imports
+import json
+import utils
 from sql.user import User
 from sql.assessment import Assessment
 from sql.session import Session
-import json
 
 #Format of JSON -KP
 #requestType: getAssessment
-#name: assessment name      (string)
-#user: username             (string)
-#section: section name      (string)
-#course: course name        (string)
-#question: question id      (int)
+#course: "string"
+#name: "string"
+#question: integer
+#section: "string"
+#session: integer
+#user: "string"
 
 def iChooseU(form):
     thisUser = utils.findUser(form)
 
-    name = form.getlist("name")[0]
-    user = form.getlist("user")[0]
-    section = form.getlist("section")[0]
     course = form.getlist("course")[0]
+    name = form.getlist("name")[0]
     question = form.getlist("question")[0]
+    section = form.getlist("section")[0]
+    session = form.getlist("session")[0]
+    user = form.getlist("user")[0]
 
     complete = []
     count = 0
 
+    if not course == "":
+        complete += Assessment.get(0, Course.get(0, course))
+        count += 1
     if not name == "":
         complete += Assessment.get(0, name)
         count += 1
-    if not user == "":
-        complete += Assessment.get(0, User.get(0, None, user))
+    if not question == "":
+        complete += Assessment.get(0, Question.get(0, question))
         count += 1
     if not section == "":
         complete += Assessment.get(0, Section.get(0, section))
         count += 1
-    if not course == "":
-        complete += Assessment.get(0, Course.get(0, course))
+    if not session == "":
+        complete += Assessment.get(0, Session.get(0, session))
         count += 1
-    if not question == "":
-        complete += Assessment.get(0, Question.get(0, question))
+    if not user == "":
+        complete += Assessment.get(0, User.get(0, None, user))
         count += 1
 
     collect = []
