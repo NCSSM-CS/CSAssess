b6078d986dab49b938f7e44a3fa50e0093242a63
Keshav Patel <patel15k2@ncssm.edu>
2015-03-06 11:23:48 -0500

    fixed spelling error

--- controller/getUser.py
+++ controller/getUser.py
@@ -39,7 +39,7 @@ def iChooseU(form):
     if not section == "":
         complete += User.get(0, section)
         count += 1
-    if not Assessment == "":
+    if not assessment == "":
         complete += User.get(0, assessment)
         count += 1
 
