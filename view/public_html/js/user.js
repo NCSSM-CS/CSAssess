2b25d8bbabdb5b8700331f0954491e2e35509637
Andrew Spencer <spencer16a@ncssm.edu>
2015-03-06 10:04:37 -0500

    fixing user.js

---
+++ view/public_html/js/user.js
@@ -0,0 +1,32 @@
+/* 
+ * This is a file to test the functionality of getting a user. 
+ */
+
+function generateUserInfo() {
+    var firstName = $("#firstName").val();
+    var lastName = $("lastName").val();
+    var token = getCookie("token");
+    var dataDef = {"requestType":"getUser", "session": token, "firstName": firstName, "lastName": lastName};
+    var urlDef = "/cgi-bin/CSAssess/request.py";
+    var dataTypeDef = "json";
+    $.post(urlDef, dataDef, showUser, dataTypeDef);
+}
+
+function showUser(users) {
+    var length = users.length;
+     var tbody = document.createElement("TBODY"); //gets all questions and puts them in table 
+	for (var i = 0; i < length; i++) 
+	{	
+		var tr = document.createElement("TR");
+                data = Object.getKeys(users[i]);
+		for (var j = 0; j < data.length; j++) 
+		{
+    		var td = document.createElement("TD");
+    		td.textContent = users[i][j];
+    		tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+    database.appendChild(tbody);
+    document.getElementById("userContainer").appendChild(tbody);
+}
