f2ca6745964a5670d756b24147dc0becdaab5ba7
Micah Halter <micah@mehalter.com>
2015-03-06 12:09:07 -0500

    fix aninda's mess up

--- controller/sql/question.py
+++ controller/sql/question.py
@@ -133,7 +133,8 @@ class Question:
             cnx = mysql.connector.connect(**getConfig())
             cursor = cnx.cursor()
 
-            insert = ("INSERT INTO question (created_by, language, type, difficulty, prev_question_id, version_number, last_given, content, active) VALUES (%s, '%s', '%s', %s, %s, %s, '%s', '%s', %s);" % (self.created_by.id, self.language, self.atype, self.difficulty, self.prev_question.id, self.version_number, self.last_given, self.content, self.active))
+            insert = ("INSERT INTO question (created_by, language, type, difficulty, prev_question_id, version_number, last_given, content, active) VALUES (%s, '%s', '%s', %s, %s, %s, '%s', '%s', %s);" % (self.created_by.id, self.language, self.atype, self.difficulty, (self.prev_question.id if type(self.prev_question.id) is Question else "Null"), self.version_number, self.last_given, self.content, self.active))
+            print(insert)
             cursor.execute(insert)
 
             select = "SELECT LAST_INSERT_ID();"
