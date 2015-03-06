983768d0898e1d1aa771dedd5119fafb37721864
anindamanocha <manocha15a@ncssm.edu>
2015-03-06 15:28:30 -0500

    commented out variables

--- controller/getAssessment.py
+++ controller/getAssessment.py
@@ -26,34 +26,34 @@ from sql.session import Session
 def iChooseU(form):
     thisUser = utils.findUser(form)
 
-    course = form.getlist("course")[0]
+#    course = form.getlist("course")[0]
     name = form.getlist("name")[0]
-    question = form.getlist("question")[0]
-    section = form.getlist("section")[0]
-    session = form.getlist("session")[0]
-    user = form.getlist("user")[0]
+#    question = form.getlist("question")[0]
+#    section = form.getlist("section")[0]
+#    session = form.getlist("session")[0]
+#    user = form.getlist("user")[0]
 
     complete = []
     count = 0
 
-    if not course == "":
-        complete += Assessment.get(0, Course.get(0, course))
-        count += 1
+#    if not course == "":
+#        complete += Assessment.get(0, Course.get(0, course))
+#        count += 1
     if not name == "":
         complete += Assessment.get(0, name)
         count += 1
-    if not question == "":
-        complete += Assessment.get(0, Question.get(0, question))
-        count += 1
-    if not section == "":
-        complete += Assessment.get(0, Section.get(0, section))
-        count += 1
-    if not session == "":
-        complete += Assessment.get(0, Session.get(0, session))
-        count += 1
-    if not user == "":
-        complete += Assessment.get(0, User.get(0, None, user))
-        count += 1
+#    if not question == "":
+#        complete += Assessment.get(0, Question.get(0, question))
+#        count += 1
+#    if not section == "":
+#        complete += Assessment.get(0, Section.get(0, section))
+#        count += 1
+#    if not session == "":
+#        complete += Assessment.get(0, Session.get(0, session))
+#        count += 1
+#    if not user == "":
+#        complete += Assessment.get(0, User.get(0, None, user))
+#        count += 1
 
     collect = []
     intersect = []
