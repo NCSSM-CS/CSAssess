d888e49c7f6f42c222b60aee8078dc3d5f6e61bb
Ebube Chuba <chuba15e@ncssm.edu>
2015-03-06 11:17:41 -0500

    Fixed namespace error

--- controller/utils.py
+++ controller/utils.py
@@ -6,18 +6,20 @@
 # Last modified date: 3/6/2014
 
 import os
+import json
 from sql.user import User
 from sql.session import Session
 
-def findUser(json):
+def findUser(form):
     return User.get(1)[0]
-    session = Session.get(json["session"], )[0]
+    ipAddress = "1234567.23"
+    session = Session.get(json["session"],ipAddress )[0]
     thisUser = session.user
     if DEBUG > 1:
         print(thisUser)
     return thisUser
 
-def successJson(json):
+def successJson(form):
     session = Session.get("1234567890123456789012345678901234567890123456789012345678901234", "127.0.0.1")[0]
     successJson = json.dumps({"success":True, "session":session.toJson()})
     return successJson
