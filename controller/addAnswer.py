050a4944616a1926f851b5f1376d638fcfefb265
anindamanocha <manocha15a@ncssm.edu>
2015-03-06 10:02:10 -0500

    fix addAnswer

--- controller/addAnswer.py
+++ controller/addAnswer.py
@@ -4,21 +4,21 @@
 created_by:         Aninda Manocha
 created_date:       3/4/2015
 last_modified_by:   Aninda Manocha
-last_modified date: 3/5/2015
+last_modified date: 3/6/2015
 """
 
 # imports
 import constants
+import utils
 import json
 from sql.user import User
 from sql.question import Question
 from sql.answer import Answer
-from sql.topic import Topic
 from sql.session import Session
 
 #Format of answer -AM
-#requestType: answer
-#question: Question
+#requestType: addAnswer
+#question: integer
 #content: "string"
 #isSolution: boolean
 
@@ -26,11 +26,11 @@ def iChooseU(json):
     thisUser = utils.findUser(json)
     
     question = json["question"]
-    theQuestion = Question.get(question["id"])[0]
+    thisQuestion = Question.get(question)[0]
     content = json["content"]
     isSolution = True 
 
-    newAnswer = Answer.noID(None, thisUser, theQuestion, None, content, isSolution, ACTIVE)
+    newAnswer = Answer.noID(None, thisUser, thisQuestion, None, content, isSolution, ACTIVE)
     newAnswer.add()
 
     return utils.successJson(json)
